08cd379d3c9cd3de97584663235a8c77d49a6444
mayur <mayurdesai9600>
2022-12-24 18:11:03 +0530

    red

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-2"
+    bucket = "batch-15"
     key = "main"
-    region = "ap-south-1"
-    dynamodb_table = "my-dynamodb-table"
+    region = "us-east-1"
+    dynamodb_table = "batch-15"
   }
 }
